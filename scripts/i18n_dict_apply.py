f458628cb45112c45dc2113fe23aa88697c65deb
Futrime <futrime@outlook.com>
2023-03-10 22:28:03 +0800

    feat: add i18n application script

---
+++ scripts/i18n_dict_apply.py
@@ -0,0 +1,36 @@
+import argparse
+import json
+import os
+
+def main():
+    script_path = os.path.dirname(os.path.realpath(__file__))
+
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='Apply a dictionary for i18n')
+    parser.add_argument('lang', type=str, help='Language code')
+    args = parser.parse_args()
+
+    # Load the dictionary
+    entries = []
+
+    with open(os.path.join(script_path, '..', 'i18n', args.lang + '.json'), 'r', encoding='utf-8') as f:
+        entries = json.load(f)
+
+    # Apply the dictionary
+    for file in os.listdir(os.path.join(script_path, '..', 'include')):
+        if not file.endswith('.h'):
+            continue
+
+        with open(os.path.join(script_path, '..', 'include', file), 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+
+        for i in range(len(lines)):
+            for entry in entries:
+                if f"/// {entry['type']} {entry['key']}" in lines[i]:
+                    lines[i] = lines[i].replace(entry['key'], entry[args.lang])
+
+        with open(os.path.join(script_path, '..', 'include', file), 'w', encoding='utf-8') as f:
+            f.writelines(lines)
+
+if __name__ == '__main__':
+    main()
