0ba13fbb6810d831465fd1861594086da8a52799
Futrime <futrime@outlook.com>
2023-03-17 11:58:51 +0800

    feat: add block source

--- include/block_source.h
+++ include/block_source.h
@@ -19,7 +19,7 @@ typedef struct ncsdk_BlockSource ncsdk_BlockSource;
 /// @param self The block source.
 /// @param position The position.
 /// @return The block, or NULL if not found.
-const ncsdk_Block* ncsdk_BlockSource_Get(const ncsdk_BlockSource* self,
+const ncsdk_Block* ncsdk_BlockSource_GetBlock(const ncsdk_BlockSource* self,
                                          const ncsdk_Position(int) * position);
 
 #ifdef __cplusplus
---
+++ include/internal/section.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "block.h"
+#include "position.h"
+
+struct ncsdk_Section {
+  ncsdk_Position(int) position;
+  int block_id_array[16][16][16];
+};
+
+typedef struct ncsdk_Section ncsdk_Section;
+
+ncsdk_Section* ncsdk_Section_New(const ncsdk_Position(int) * position,
+                                 int block_id_array[16][16][16]);
+
+void ncsdk_Section_Delete(ncsdk_Section* self);
+
+const ncsdk_Block* ncsdk_Section_GetBlock(const ncsdk_Section* self,
+                                          const ncsdk_Position(int) * position);
+
+const ncsdk_Position(int) *
+    ncsdk_Section_GetPosition(const ncsdk_Section* self);
+
+#ifdef __cplusplus
+}
+#endif
