1a7e6caf11d5bdbd696d60b2b3446db51435ab51
Futrime <futrime@outlook.com>
2023-03-17 12:13:09 +0800

    feat: add section

--- include/internal/section.h
+++ include/internal/section.h
@@ -11,7 +11,6 @@ struct ncsdk_Section {
   ncsdk_Position(int) position;
   int block_id_array[16][16][16];
 };
-
 typedef struct ncsdk_Section ncsdk_Section;
 
 ncsdk_Section* ncsdk_Section_New(const ncsdk_Position(int) * position,
@@ -20,10 +19,15 @@ ncsdk_Section* ncsdk_Section_New(const ncsdk_Position(int) * position,
 void ncsdk_Section_Delete(ncsdk_Section* self);
 
 const ncsdk_Block* ncsdk_Section_GetBlock(const ncsdk_Section* self,
-                                          const ncsdk_Position(int) * position);
+                                          const ncsdk_Position(int) *
+                                              relative_position);
 
 const ncsdk_Position(int) *
     ncsdk_Section_GetPosition(const ncsdk_Section* self);
+
+void ncsdk_Section_SetBlock(ncsdk_Section* self,
+                            const ncsdk_Position(int) * relative_position,
+                            const ncsdk_Block* block);
 
 #ifdef __cplusplus
 }
